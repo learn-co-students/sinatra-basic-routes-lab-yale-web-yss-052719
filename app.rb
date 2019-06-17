4f590cab3aad2d74572c5f9fdee65f935c31b75c
maxsunnyday <max.sun@yale.edu>
2019-06-16 23:25:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Max"
+    end
+
+    get '/hometown' do
+        "My hometown is Corvallis"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Old Town Road"
+    end
 end
